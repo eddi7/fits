39475ea00a9d5004c2c2edf107dcb53782732b26
eddi7 <47506207+eddi7@users.noreply.github.com>
2025-12-08 21:11:38 +0800

    Update daily_coverage_change.sql

--- daily_coverage_change.sql
+++ daily_coverage_change.sql
@@ -9,16 +9,23 @@ WITH totals AS (
     FROM v_latest_branches_coverage_results
 )
 SELECT
-    CASE
-        WHEN latest_total IS NULL OR latest_total = 0 THEN NULL
-        ELSE latest_hit / latest_total
-    END AS latest_coverage,
-    CASE
-        WHEN previous_total IS NULL OR previous_total = 0 THEN NULL
-        ELSE previous_hit / previous_total
-    END AS previous_coverage,
-    CASE
-        WHEN latest_total IS NULL OR latest_total = 0 OR previous_total IS NULL OR previous_total = 0 THEN NULL
-        ELSE (latest_hit / latest_total) - (previous_hit / previous_total)
-    END AS coverage_change
+    CAST(
+        CASE
+            WHEN latest_total IS NULL OR latest_total = 0 THEN NULL
+            ELSE CAST(latest_hit AS DECIMAL(38, 10)) / latest_total
+        END AS DECIMAL(11, 10)
+    ) AS latest_coverage,
+    CAST(
+        CASE
+            WHEN previous_total IS NULL OR previous_total = 0 THEN NULL
+            ELSE CAST(previous_hit AS DECIMAL(38, 10)) / previous_total
+        END AS DECIMAL(11, 10)
+    ) AS previous_coverage,
+    CAST(
+        CASE
+            WHEN latest_total IS NULL OR latest_total = 0 OR previous_total IS NULL OR previous_total = 0 THEN NULL
+            ELSE (CAST(latest_hit AS DECIMAL(38, 10)) / latest_total)
+                 - (CAST(previous_hit AS DECIMAL(38, 10)) / previous_total)
+        END AS DECIMAL(11, 10)
+    ) AS coverage_change
 FROM totals;
